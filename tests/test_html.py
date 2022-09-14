d3b52c8e97d81f394186b0c3d3b312167b346915
Jonathan Sick <jonathan@jsick.codes>
2022-09-14 10:58:20 -0400

    Drop auto fail on the smoke test

--- tests/test_html.py
+++ tests/test_html.py
@@ -10,7 +10,10 @@ from .lib.helpers import write_conversion
 
 
 def test_html_export() -> None:
+    """Smoke test for the HTML export.
+
+    Output is written to _build/html_export.
+    """
     test_notebook = Path(__file__).parent.joinpath("color-excess.ipynb")
     html, resources = LearnAstropyHtmlExporter().from_filename(test_notebook)
     write_conversion(base_dir="html_export", content=html, resources=resources)
-    assert False
