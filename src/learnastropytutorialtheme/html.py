3d0c8850c176a46580e0578fbe3ace87d63c90d6
Jonathan Sick <jonathan@jsick.codes>
2023-09-25 11:19:52 -0400

    Fix setting extra_template_basedirs
    We can't override TemplateExporter's extra_template_basedirs attribute
    with a property; instead set it from the constructor.

--- src/learnastropytutorialtheme/html.py
+++ src/learnastropytutorialtheme/html.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Any, Dict, List
+from typing import Any, Dict
 
 from nbconvert.exporters.html import HTMLExporter
 
@@ -13,15 +13,17 @@ class LearnAstropyHtmlExporter(HTMLExporter):
 
     export_from_notebook = "Learn Astropy HTML"
 
-    @property
-    def extra_template_basedirs(self) -> List[str]:
-        """Include this package's built-in template in the search path."""
-        _paths = super()._default_extra_template_basedirs()
-        _paths.append(self._template_name_default())
-        return _paths
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+        # Add the default template to the search path
+        self.extra_template_basedirs.append(self._template_name_default())
 
     def _template_name_default(self) -> str:
-        """Select built-in HTML theme as the default."""
+        """Select built-in HTML theme as the default.
+
+        Overrides `HTMLExporter._template_name_default`.
+        """
         return str(
             Path(__file__).parent.joinpath("templates").joinpath("html")
         )
