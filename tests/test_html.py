78fa77f83db83ed89662ac1a89d73e154a650e81
Jonathan Sick <jonathan@jsick.codes>
2023-09-25 12:22:50 -0400

    Set links for nav links via resources
    This begins to establish a schema for custom keys in the nbconvert
    resources dictionary, which is available in the Jinja templates.
    nbcollection can define these resources when exporting a notebook to
    HTML with this template.

--- tests/test_html.py
+++ tests/test_html.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from copy import deepcopy
 from pathlib import Path
 
 from learnastropytutorialtheme.html import LearnAstropyHtmlExporter
@@ -18,4 +19,16 @@ def test_html_export() -> None:
     html, resources = LearnAstropyHtmlExporter().from_filename(
         str(test_notebook.resolve())
     )
+    resources = deepcopy(resources)
+    resources["learn_astropy_editor_url"] = (
+        "https://mybinder.org/v2/gh/astropy/astropy-tutorials/"
+        "main?labpath=tutorials%2Fcolor-excess%2Fcolor-excess.ipynb"
+    )
+    resources["learn_astropy_source_url"] = (
+        "https://github.com/astropy/astropy-tutorials/blob/main/tutorials/"
+        "color-excess/color-excess.ipynb"
+    )
+    resources[
+        "learn_astropy_ipynb_download_url"
+    ] = "https://learn.astropy.org/tutorials/color-excess.ipynb"
     write_conversion(base_dir="html_export", content=html, resources=resources)
